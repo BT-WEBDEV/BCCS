f66d4ef8acd7b8796a3e71b2c3c265b768136001
Brandon Tetrick <brandontetrick@gmail.com>
2023-08-28 14:27:35 -0600

    Initial Commit
    Just adding to the repo...

---
+++ Barrett Concrete Cutting & Services/barrett-concrete/template-parts/about-us-grid.php
@@ -0,0 +1,70 @@
+<div class='container picture-grid-container'>
+	<div class="row picture-row-wrap"> 
+		<div id="about-pic-1" class="picture-container">
+			<div class="info-container"> 
+				<h6 class="grid-heading"> SAFETY/CERTIFICATIONS/TRAINING </h6> 
+				<p class="grid-subheading"> Each member of our field staff... </p>
+			</div>
+			<div class="active-hover"> 
+				<h6 class="grid-heading"> SAFETY/CERTIFICATIONS/TRAINING </h6> 
+				<p class="grid-subheading"> Each member of our field staff is fully vaccinated and has ongoing training and education to ensure safety on our end. Our team members are certified in the following: confined space, CPR, fall protection, lifts and OSHA 30. 
+				</p>
+			</div>  
+		</div>
+		<div id="about-pic-2" class="picture-container">
+			<div class="info-container"> 
+				<h6 class="grid-heading"> RELATIONSHIPS </h6> 
+				<p class="grid-subheading"> We are proud to have great relationships... </p>
+			</div>
+			<div class="active-hover"> 
+				<h6 class="grid-heading"> RELATIONSHIPS </h6> 
+				<p class="grid-subheading"> We are proud to have great relationships in the industry that we put to work for our clients. We are determined to remain your trusted source for all things concrete cutting and beyond.  
+				</p>
+			</div> 
+		</div>
+		<div id="about-pic-3" class="picture-container">
+			<div class="info-container"> 
+				<h6 class="grid-heading"> AVAILABILITY </h6> 
+				<p class="grid-subheading"> When you work with Barrett... </p>
+			</div>
+			<div class="active-hover"> 
+				<h6 class="grid-heading"> AVAILABILITY </h6> 
+				<p class="grid-subheading"> When you work with Barrett Concrete Cutting & Services, you have a team that’s responsive, on call, and committed to making sure your questions are answered and your needs are met. Utilizing our established vendor status, we can get started on your timeline.
+				</p>
+			</div>
+		</div>
+		<div id="about-pic-4" class="picture-container">
+			<div class="info-container"> 
+				<h6 class="grid-heading"> COST </h6> 
+				<p class="grid-subheading"> With our proactive mindset... </p>
+			</div>
+			<div class="active-hover"> 
+				<h6 class="grid-heading"> COST </h6> 
+				<p class="grid-subheading"> With our proactive mindset, industry-leading tools and capabilities, plus existing relationships with business professionals, we are able to offer a range of services at a competitive and affordable price. 
+				</p>
+			</div>
+		</div>
+		<div id="about-pic-5" class="picture-container">
+			<div class="info-container"> 
+				<h6 class="grid-heading"> RELIABILITY </h6> 
+				<p class="grid-subheading">  Our goal is to always remain... </p>
+			</div>
+			<div class="active-hover"> 
+				<h6 class="grid-heading"> RELIABILITY </h6> 
+				<p class="grid-subheading"> Our goal is to always remain as close as possible to the project schedule from start to finish - using our experienced operators in concrete cutting services. When you work with Barrett Concrete, you know you can expect quality.
+				</p>
+			</div>
+		</div>
+		<div id="about-pic-6" class="picture-container">
+			<div class="info-container"> 
+				<h6 class="grid-heading"> ENVIRONMENTAL IMPACT </h6> 
+				<p class="grid-subheading"> Barrett Concrete is committed... </p>
+			</div>
+			<div class="active-hover"> 
+				<h6 class="grid-heading"> ENVIRONMENTAL IMPACT </h6> 
+				<p class="grid-subheading"> Barrett Concrete Cutting & Services is committed to minimizing our environmental impact. We're modernizing our fleet of vehicles, phasing out gas powered tools, and recycling the cores we drill whenever possible.
+				</p>
+			</div>
+		</div>
+	</div> 
+</div>
